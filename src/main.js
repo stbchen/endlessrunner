03615fd4d084ca0ddba118b5974a459ea0b9945d
stbchen <stbchen@ucsc.com>
2022-04-20 14:03:47 -0700

    main

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@
 // Stanley, Nile, Ben
 // CMPM/ARTG 120
 
-// let config = {
-//     type: Phaser.CANVAS,
-//     width: 640,
-//     height: 480,
-//     scene: [Menu, Play],
-// };
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play],
+};
 
-//let keyR, keyLEFT, keyRIGHT, keyA, keyD, keyUP, keyW;
+let keyUP;
 
-let game = new Phaser.Game();
+let game = new Phaser.Game(config);
