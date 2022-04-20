9b05491a2a172872a2cdde932cab7583e8b9c801
stbchen <stbchen@ucsc.com>
2022-04-20 13:50:12 -0700

    added main.js and more folders

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// Endless Runner: Running from the Alien Horde
+// Stanley, Nile, Ben
+// CMPM/ARTG 120
+
+// let config = {
+//     type: Phaser.CANVAS,
+//     width: 640,
+//     height: 480,
+//     scene: [Menu, Play],
+// };
+
+//let keyR, keyLEFT, keyRIGHT, keyA, keyD, keyUP, keyW;
+
+let game = new Phaser.Game();
