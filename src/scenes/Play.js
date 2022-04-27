d3353ac456d97e499145499754064284b644e023
stbchen <stbchen@ucsc.com>
2022-04-27 14:28:47 -0700

    added better collisions and player hit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -39,7 +39,8 @@ class Play extends Phaser.Scene {
         this.MAX_JUMPS = 3; // change for double/triple/etc. jumps 🤾‍♀️
         this.JUMP_VELOCITY = -700;
         this.physics.world.gravity.y = 2600;
-        
+        this.counter = 0;
+
         // make ground tiles group
         this.ground = this.add.group();
         for(let i = 0; i < game.config.width; i += tileSize/2) {
@@ -54,9 +55,10 @@ class Play extends Phaser.Scene {
         this.player.setCollideWorldBounds(true);
         this.player.setMaxVelocity(this.MAX_X_VEL, this.MAX_Y_VEL);
         this.health = 3;
+        this.iframe = 0;
 
         // Adding enemy1
-        this.enemy1 = new Enemy1(this, game.config.width, game.config.height/2, 'enemy1', 0).setOrigin(0, 0);
+        //this.enemy1 = new Enemy1(this, game.config.width, game.config.height/2, 'enemy1', 0).setOrigin(0, 0);
         
         // Adding enemy2
         //this.enemy2 = this.physics.add.sprite(0, game.config.height + 200, 'enemy2', 0).setOrigin(0, 0);
@@ -74,6 +76,20 @@ class Play extends Phaser.Scene {
         this.physics.add.collider(this.player, this.ground);
         this.physics.add.collider(this.enemy2, this.ground);
        
+        // Adding collider with enemy2
+        this.physics.add.collider(this.player, this.enemy2,() => {
+            if (this.time.now > this.iframe) {
+                this.hit();
+                this.despawn();
+                // SET IFRAMES (in seconds)
+                this.iframe = this.time.now + 10000;
+                this.player.body.velocity.x = 0;
+            } else {
+                this.despawn();
+                this.player.body.velocity.x = 0;
+            }
+        });
+    
         // Setting game over to false
         this.gameOver = false;
         this.e2appear = true;
@@ -86,32 +102,17 @@ class Play extends Phaser.Scene {
     }
     
     update() {
+
+        // Check if game is not over
         if (!this.gameOver) {
             // Moving background
             this.background.tilePositionX += 4;
 
             this.playerBack.setX(this.player.x);
             this.playerBack.setY(this.player.y);
-            
+
             //this.enemy1.update();
-            this.enemy1.update();
             
-            // check keyboard input
-
-            // if(cursors.left.isDown) {
-            //     this.player.body.setAccelerationX(-this.ACCELERATION);
-            //     this.player.setFlip(true, false);
-            //     // see: https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Components.Animation.html#play__anchor
-            //     // play(key [, ignoreIfPlaying] [, startFrame])
-            // } else if(cursors.right.isDown) {
-            //     this.player.body.setAccelerationX(this.ACCELERATION);
-            //     this.player.resetFlip();
-            // } else {
-            //     // set acceleration to 0 so DRAG will take over
-            //     this.player.body.setAccelerationX(0);
-            //     this.player.body.setDragX(this.DRAG);
-            // }
-
             // check if player is grounded
             this.player.isGrounded = this.player.body.touching.down;
             // if so, we have jumps to spare 
@@ -135,24 +136,19 @@ class Play extends Phaser.Scene {
                 this.jumping = false;
             }
 
-            if (this.checkCollision(this.player, this.enemy1)) {
-                this.hit();
-                this.enemy1.reset();
-            }
+            // if (this.checkCollision(this.player, this.enemy1)) {
+            //     this.hit();
+            //     this.health--;
+            //     this.enemy1.reset();
+            // }
 
             // Enemy2 update
-            //this.counter = 0;
             if(this.enemy2.x == 940) {
                 this.enemy2.body.setAccelerationX(-this.ACCELERATION/10);
-                //while (this.counter < 20) {
-                    this.enemy2.body.velocity.x -= 10;
-                    //this.counter++;
-                //}
-                //this.counter = 0;
+                this.enemy2.body.velocity.x -= 10;
                 // see: https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Components.Animation.html#play__anchor
                 // play(key [, ignoreIfPlaying] [, startFrame])
             } else if(this.enemy2.x == 0) {
-
                 //this.enemy2.body.setAccelerationX(this.ACCELERATION/10);
                 this.enemy2.setVisible(false);
                 this.enemy2.body.velocity.x = 0;
@@ -163,32 +159,8 @@ class Play extends Phaser.Scene {
                 this.enemy2.body.allowGravity = false;
                 this.e2appear = false;
                 this.delay = this.time.now + Phaser.Math.Between(3000, 5000);
-                //while (this.counter < 7) {
-                    // this.enemy2.body.velocity.x += 10;
-                    // console.log("left");
-                    //this.counter++;
-               // }
-                //this.counter = 0;
-            // } else {
-            //     // set acceleration to 0 so DRAG will take over
-            //     this.enemy2.body.setAccelerationX(0);
-            //     this.enemy2.body.setDragX(this.DRAG);
             }
 
-            if (this.checkCollision(this.player, this.enemy2) && this.e2appear == true) {
-                this.hit();
-                //this.enemy2.body.setAccelerationX(this.ACCELERATION/10);
-                this.enemy2.setVisible(false);
-                this.enemy2.body.velocity.x = 0;
-                this.enemy2.y = 50;
-                this.enemy2.x = 50;
-                this.enemy2.body.setAccelerationX(0);
-                this.enemy2.body.immovable = true;
-                this.enemy2.body.allowGravity = false;
-                this.e2appear = false;
-                this.delay = this.time.now + Phaser.Math.Between(3000, 5000);
-                //this.enemy2.x = 0;
-            }
 
         }
 
@@ -202,6 +174,7 @@ class Play extends Phaser.Scene {
         }
 
         // console.log(this.time.now, this.delay);
+        // reset enemy
         if (this.time.now > this.delay && this.delay != 0) {
             this.enemy2.setVisible(true);
             this.enemy2.body.immovable = false;
@@ -217,9 +190,32 @@ class Play extends Phaser.Scene {
 
     }
 
+    despawn() {
+        this.enemy2.setVisible(false);
+        this.enemy2.body.velocity.x = 0;
+        this.enemy2.y = 50;
+        this.enemy2.x = 50;
+        this.enemy2.body.setAccelerationX(0);
+        this.enemy2.body.immovable = true;
+        this.enemy2.body.allowGravity = false;
+        this.e2appear = false;
+        this.delay = this.time.now + Phaser.Math.Between(3000, 5000);
+    }
     hit() {
-        this.health--;
-        this.player.x -= 150;
+        if (this.counter < 20) {
+            this.player.x -= 5;
+            this.counter++;    
+        } else {
+            this.counter = 0;
+            this.health--;
+            return;
+        }
+        this.time.delayedCall(1, this.hit, [], this);
+    }
+
+    moveBack() {
+        this.player.x -= 1;
+        console.log("hi");
     }
 
     lookBack() {
@@ -234,15 +230,18 @@ class Play extends Phaser.Scene {
         }
         this.time.delayedCall(500, this.lookBack, [], this);    // delayed recursive calls
     }
-    checkCollision(player, enemy) {
-        // simple AABB checking
-        if (player.x < enemy.x + enemy.width && 
-            player.x + player.width > enemy.x && 
-            player.y < enemy.y + enemy.height &&
-            player.height + player.y > enemy. y) {
-                return true;
-        } else {
-            return false;
-        }
-    }
 }
+
+// if(cursors.left.isDown) {
+//     this.player.body.setAccelerationX(-this.ACCELERATION);
+//     this.player.setFlip(true, false);
+//     // see: https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Components.Animation.html#play__anchor
+//     // play(key [, ignoreIfPlaying] [, startFrame])
+// } else if(cursors.right.isDown) {
+//     this.player.body.setAccelerationX(this.ACCELERATION);
+//     this.player.resetFlip();
+// } else {
+//     // set acceleration to 0 so DRAG will take over
+//     this.player.body.setAccelerationX(0);
+//     this.player.body.setDragX(this.DRAG);
+// }
