19eee7cf895cd9d81fed490ec312c2c7b5d5549c
bputaski <91684470+bputaski@users.noreply.github.com>
2022-05-02 13:25:50 -0500

    fixed random spawns?

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -297,7 +297,7 @@ class Play extends Phaser.Scene {
 
     spawn(enemy) {
         enemy.setVisible(true);
-        //this.enemy1.body.y = getRandomInt(20,300); //change what range enemy1 will spawn in
+        this.enemy.body.y = getRandomInt(20,300); //change what range enemy will spawn in
         //this.time.now + 6000;
         enemy.body.immovable = false;
         enemy.x = 50;
